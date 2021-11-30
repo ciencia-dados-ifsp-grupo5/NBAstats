62fe6a760b63552281b714653ba63503fb5f3d30
Diego Assis <diego.assis@gmail.com>
2021-11-29 22:09:50 -0300

    feat(deploy): working deploy version
    Deploy version with requests to predict and query player.
    
    Make request queries to a mysql database to player data.

---
+++ deploy/backend/api/player.py
@@ -0,0 +1,36 @@
+from flask import Blueprint, jsonify, current_app
+
+player_api = Blueprint('player_api', __name__)
+
+class Player():
+
+    def player_list(self, db):
+        player = db.query('''                     
+            SELECT PLAYER_ID, PLAYER_NAME, group_concat(SEASON_ID) AS SEASONS
+            FROM player
+            GROUP BY PLAYER_ID, PLAYER_NAME;
+        ''')
+        for p in player:
+            p['SEASONS'] = sorted(p['SEASONS'].split(','))
+        return jsonify(player), 200
+      
+    def player_data(self, db, player_id, season_id):
+        player = db.query(f'''
+            SELECT *
+            FROM player
+            WHERE PLAYER_ID = {player_id} AND SEASON_ID = '{season_id}'
+        ''')
+        return jsonify(player[0] if len(player) > 0 else {}), 200
+
+@player_api.route("/list", methods=['GET'])
+def player_list():
+    player_handler = Player()
+    db = current_app.config['connector']
+    return player_handler.player_list(db)
+
+
+@player_api.route("/data/<player_id>/<season_id>")
+def player_data(player_id, season_id):
+    player_handler = Player()
+    db = current_app.config['connector']
+    return player_handler.player_data(db, player_id, season_id)
