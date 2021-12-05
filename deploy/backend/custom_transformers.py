497336b3e37d31c6121e5756faa61b642d65604d
Diego Assis <diego.assis@gmail.com>
2021-12-05 20:44:57 -0300

    fix: minor fixes in transformer and sagemaker comments

--- deploy/backend/custom_transformers.py
+++ deploy/backend/custom_transformers.py
@@ -8,8 +8,7 @@ class FeatureSelector(BaseEstimator, TransformerMixin):
         return self
     
     def transform(self, X):
-        X = pd.DataFrame(X).copy()
-        X_new = X[['GS', 'PLAYER_AGE', 'FG_PCT', 'FG3A']]
+        X_new = X[['GS', 'PLAYER_AGE', 'FG_PCT', 'FG3A']].copy()
         X_new['REB/MIN'] = X['REB'] / X['MIN'].apply(lambda x: 1 if x == 0 else x)
         X_new['AST/MIN'] = X['AST'] / X['MIN'].apply(lambda x: 1 if x == 0 else x)
         X_new['PTS/MIN'] = X['PTS'] / X['MIN'].apply(lambda x: 1 if x == 0 else x)
